45a4ed28bfa2ea95ed6adee6853e31f5a200e4db
Carlos <carlosevmateus@gmail.com>
2024-04-18 19:57:36 +0100

    changes on request

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,12 +1,16 @@
 import { Network, Station } from "../types";
+import axios, { AxiosResponse } from "axios";
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
 
 export const fetchNetworks= async (): Promise<Network[] | []> =>{
     try {
-        const response = await fetch(
+        const response: AxiosResponse = await axios.get(
             "https://api.citybik.es/v2/networks"
         );
-        const data = await response.json();
-        return data.networks;
+        //const data = await response.json();
+        return response.data.networks;
     } catch (error) {
         console.error("Error fetching network:", error);
         throw error;
@@ -15,11 +19,11 @@ export const fetchNetworks= async (): Promise<Network[] | []> =>{
 
 export const fetchNetworkById = async (networkId: string): Promise<Station[]> => {
     try {
-        const response = await fetch(
+        const response: AxiosResponse = await axios.get(
             `https://api.citybik.es/v2/networks/${networkId}?fields=stations`
         );
-        const data = await response.json();
-        return data.network.stations;
+        // const data = await response.json();
+        return response.data.network.stations;
     } catch (error) {
         console.error("Error fetching network by Id:", error);
         throw error;
